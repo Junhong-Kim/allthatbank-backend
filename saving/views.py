b4f0fecb962e9d4608cdb204972524ef72de03c7
Junhong-Kim <jhk1005jhk@gmail.com>
2018-02-19 11:16:16 +0900

    Added, SavingProduct list params

--- saving/views.py
+++ saving/views.py
@@ -10,17 +10,33 @@ class SavingProductList(APIView):
     def get(self, request):
         data = {}
 
-        # 적금상품 기본
+        # 권역 코드, 금융회사 코드
+        top_fin_grp_no = request.query_params.get('top_fin_grp_no')
+        fin_co_no = request.query_params.get('fin_co_no')
+
+        # 전체 적금상품 쿼리셋
         saving_products_base_queryset = SavingProductBase.objects.all()
-        saving_products_base_serializer = SavingProductBaseSerializer(saving_products_base_queryset, many=True)
 
-        # 적금상품 옵션
-        saving_products_option_queryset = SavingProductOption.objects.all()
-        saving_products_option_serializer = SavingProductOptionSerializer(saving_products_option_queryset, many=True)
+        if top_fin_grp_no:
+            # 특정 권역 코드의 적금상품 리스트
+            qs = saving_products_base_queryset.filter(top_fin_grp_no=top_fin_grp_no)
+            serializer = SavingProductBaseSerializer(qs, many=True)
 
-        # 적금상품 응답
-        data['products'] = saving_products_base_serializer.data
-        data['options'] = saving_products_option_serializer.data
+            data['products'] = serializer.data
+
+        elif fin_co_no:
+            # 특정 금융회사 코드의 적금상품 리스트
+            qs = saving_products_base_queryset.filter(fin_co_no=fin_co_no)
+            serializer = SavingProductBaseSerializer(qs, many=True)
+
+            data['products'] = serializer.data
+
+        else:
+            # 전체 적금상품 리스트
+            serializer = SavingProductBaseSerializer(saving_products_base_queryset, many=True)
+
+            data['products'] = serializer.data
+
         return Response(data)
 
 
