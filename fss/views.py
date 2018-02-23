23d9ec0f357d2470bb1344713422bc0dae783526
Junhong-Kim <jhk1005jhk@gmail.com>
2018-02-23 14:44:48 +0900

    Added, SavingProduct data store multiprocessing

--- fss/views.py
+++ fss/views.py
@@ -1,4 +1,5 @@
 from . import services
+from multiprocessing import Process
 from saving.serializers import SavingProductBaseSerializer, SavingProductOptionSerializer
 
 from rest_framework.decorators import api_view
@@ -16,20 +17,28 @@ def saving_products(request):
         saving_products_base = response['result']['baseList']
         saving_products_option = response['result']['optionList']
 
-        for saving_product_base in saving_products_base:
-            saving_product_base['top_fin_grp_no'] = top_fin_grp_no
-
-            serializer = SavingProductBaseSerializer(data=saving_product_base)
-            if serializer.is_valid():
-                serializer.save()
-            else:
-                raise Exception(serializer.errors)
-
-        for saving_product_option in saving_products_option:
-            serializer = SavingProductOptionSerializer(data=saving_product_option)
-            if serializer.is_valid():
-                serializer.save()
-            else:
-                raise Exception(serializer.errors)
+        Process(target=saving_products_base_process, args=(saving_products_base, top_fin_grp_no)).start()
+        Process(target=saving_products_option_process, args=(saving_products_option,)).start()
 
         return Response(response)
+
+
+def saving_products_base_process(saving_products_base, top_fin_grp_no):
+    for saving_product_base in saving_products_base:
+        saving_product_base['top_fin_grp_no'] = top_fin_grp_no
+        serializer = SavingProductBaseSerializer(data=saving_product_base)
+        if serializer.is_valid():
+            serializer.save()
+        else:
+            raise Exception(serializer.errors)
+    return
+
+
+def saving_products_option_process(saving_products_option):
+    for saving_product_option in saving_products_option:
+        serializer = SavingProductOptionSerializer(data=saving_product_option)
+        if serializer.is_valid():
+            serializer.save()
+        else:
+            raise Exception(serializer.errors)
+    return
