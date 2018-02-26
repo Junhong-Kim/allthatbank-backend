4c3cda3ea1929d36fc1213def669052abe8a2d66
Junhong-Kim <jhk1005jhk@gmail.com>
2018-02-26 15:54:51 +0900

    Added, account_number field to Bankbook model

--- bankbook/models.py
+++ bankbook/models.py
@@ -14,6 +14,8 @@ class Bankbook(models.Model):
     saving_product_base_id = models.ForeignKey(SavingProductBase, on_delete=models.CASCADE)
     # 적금상품 옵션 id
     saving_product_option_id = models.ForeignKey(SavingProductOption, on_delete=models.CASCADE)
+    # 계좌번호
+    account_number = models.CharField(max_length=64)
     # 납입 주기 (D: 매일, W: 매주, M: 매월)
     payment_period = models.CharField(max_length=64)
     # 납입 일 (null: 매일, W: 요일[MON|TUE|WED|THU|FRI], M: 일[1~31])
