1bcda540c2f4e03dcd90b83171fb465a34fe61c9
Junhong-Kim <jhk1005jhk@gmail.com>
2018-02-26 18:05:25 +0900

    Added, my bankbook list view

--- bankbook/views.py
+++ bankbook/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.decorators import list_route
+from rest_framework.response import Response
 
 from bankbook.models import Bankbook
 from bankbook.serializers import BankbookSerializer
@@ -7,3 +9,15 @@ from bankbook.serializers import BankbookSerializer
 class BankbookViewSet(viewsets.ModelViewSet):
     queryset = Bankbook.objects.all()
     serializer_class = BankbookSerializer
+
+    @list_route()
+    def me(self, request):
+        """
+        내 통장 리스트
+        GET /bankbooks/me/
+        """
+        user_id = request.META.get('HTTP_USER_ID')
+        qs = self.get_queryset().filter(user_id=user_id)
+        serializer = self.get_serializer(qs, many=True)
+
+        return Response(serializer.data)
