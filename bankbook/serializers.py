c9a09dbf375c059f0854a45a557ca35b542b76eb
Junhong-Kim <jhk1005jhk@gmail.com>
2018-03-02 16:18:19 +0900

    Added, AESCipher

--- bankbook/serializers.py
+++ bankbook/serializers.py
@@ -1,3 +1,4 @@
+from allthatbank.utils.cipher import AESCipher
 from rest_framework import serializers
 from .models import Bankbook
 
@@ -6,3 +7,17 @@ class BankbookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Bankbook
         fields = '__all__'
+
+    def to_internal_value(self, data):
+        # 계좌번호 암호화
+        ret = super(BankbookSerializer, self).to_internal_value(data)
+        ret['account_number'] = AESCipher().encrypt_str(ret['account_number'])
+
+        return ret
+
+    def to_representation(self, instance):
+        # 계좌번호 복호화
+        ret = super(BankbookSerializer, self).to_representation(instance)
+        ret['account_number'] = AESCipher().decrypt_str(ret['account_number'])
+
+        return ret
