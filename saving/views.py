e759455368b15159ca4d0755f93d51781a760b33
Junhong-Kim <jhk1005jhk@gmail.com>
2018-07-09 00:24:08 +0900

    Modify: search for saving-products by option

--- saving/views.py
+++ saving/views.py
@@ -1,5 +1,3 @@
-import math
-
 from django.db.models import Q
 from django.http import Http404
 from rest_framework import status
@@ -13,8 +11,11 @@ from saving.serializers import SavingProductBaseSerializer, SavingProductOptionS
 
 
 class SavingProduct(object):
-    def set_options_data(self, options):
-        option_data = {
+    def set_custom_product_data(self, product):
+        company = CompanyBase.objects.get(fin_co_no=product['fin_co_no'])
+        options = product['options']
+
+        custom_option_data = {
             'basic_rate': {},
             'prime_rate': {},
             'save_trm': set(),
@@ -28,9 +29,9 @@ class SavingProduct(object):
         for option in options:
             basic_rates.append(option['intr_rate'])
             prime_rates.append(option['intr_rate2'])
-            option_data['save_trm'].add(option['save_trm'])
-            option_data['rate_type'].add(option['intr_rate_type'])
-            option_data['rsrv_type'].add(option['rsrv_type'])
+            custom_option_data['save_trm'].add(option['save_trm'])
+            custom_option_data['rate_type'].add(option['intr_rate_type'])
+            custom_option_data['rsrv_type'].add(option['rsrv_type'])
 
         # 기본금리가 null 값인 경우 0으로 초기화
         for (index, basic_rate) in enumerate(basic_rates):
@@ -45,18 +46,37 @@ class SavingProduct(object):
         basic_rates = sorted(basic_rates, key=float)
         prime_rates = sorted(prime_rates, key=float)
 
-        option_data['basic_rate']['min'] = basic_rates[-1] if basic_rates[0] == 0 else basic_rates[0]
-        option_data['basic_rate']['max'] = basic_rates[0] if basic_rates[-1] == 0 else basic_rates[-1]
-        option_data['prime_rate']['min'] = prime_rates[-1] if prime_rates[0] == 0 else prime_rates[0]
-        option_data['prime_rate']['max'] = prime_rates[0] if prime_rates[-1] == 0 else prime_rates[-1]
+        custom_option_data['basic_rate']['min'] = basic_rates[-1] if basic_rates[0] == 0 else basic_rates[0]
+        custom_option_data['basic_rate']['max'] = basic_rates[0] if basic_rates[-1] == 0 else basic_rates[-1]
+        custom_option_data['prime_rate']['min'] = prime_rates[-1] if prime_rates[0] == 0 else prime_rates[0]
+        custom_option_data['prime_rate']['max'] = prime_rates[0] if prime_rates[-1] == 0 else prime_rates[-1]
 
-        return option_data
+        custom_product_data = {
+            'id': product['fin_prdt_cd'],
+            'bank_logo': 'logo.png',
+            'bank_name': company.kor_co_nm,
+            'join_way': product['join_way'],
+            'product_name': product['fin_prdt_nm'],
+            'basic_rate_min': custom_option_data['basic_rate']['min'],
+            'basic_rate_max': custom_option_data['basic_rate']['max'],
+            'prime_rate_min': custom_option_data['prime_rate']['min'],
+            'prime_rate_max': custom_option_data['prime_rate']['max'],
+            'months_06': '6' in custom_option_data['save_trm'],
+            'months_12': '12' in custom_option_data['save_trm'],
+            'months_24': '24' in custom_option_data['save_trm'],
+            'months_36': '36' in custom_option_data['save_trm'],
+            'rate_type_s': 'S' in custom_option_data['rate_type'],
+            'rate_type_m': 'M' in custom_option_data['rate_type'],
+            'rsrv_type_s': 'S' in custom_option_data['rsrv_type'],
+            'rsrv_type_f': 'F' in custom_option_data['rsrv_type'],
+            'join_deny': product['join_deny'],
+            'join_member': product['join_member']
+        }
+        return custom_product_data
 
 
 class SavingProductList(APIView, SavingProduct):
     def get(self, request):
-        data = {}
-
         # 페이징
         offset = int(request.query_params.get('offset', 1))
         limit = int(request.query_params.get('limit', 10))
@@ -78,74 +98,50 @@ class SavingProductList(APIView, SavingProduct):
             """
             qs = saving_products_base_queryset.filter(top_fin_grp_no=top_fin_grp_no)
             serializer = SavingProductBaseSerializer(qs, many=True)
-            data = serializer.data[start_index:end_index]
+            products = serializer.data[start_index:end_index]
         elif fin_co_no:
             """
             특정 금융회사 코드의 적금상품 리스트
             GET /saving_products?fin_co_no=&fin_co_no=offset=&limit=
             """
-            data = []
+            products = []
             fin_co_nos = request.query_params.getlist('fin_co_no')
             for fin_co_no in fin_co_nos:
                 qs = saving_products_base_queryset.filter(fin_co_no=fin_co_no)
                 serializer = SavingProductBaseSerializer(qs, many=True)
-                data += serializer.data
-            data = data[start_index:end_index]
+                products += serializer.data
+            products = products[start_index:end_index]
 
             # 적금상품 옵션
-            for product in data:
+            for product in products:
                 fin_prdt_cd = product['fin_prdt_cd']
                 saving_product_option_queryset = SavingProductOption.objects.all().filter(fin_prdt_cd=fin_prdt_cd)
                 saving_product_option_serializer = SavingProductOptionSerializer(saving_product_option_queryset,
                                                                                  many=True)
                 product['options'] = saving_product_option_serializer.data
 
-            products = []
-            for product in data:
-                company = CompanyBase.objects.get(fin_co_no=product['fin_co_no'])
-                options = product['options']
-                options_data = self.set_options_data(options)
-
-                product_data = {
-                    'id': product['fin_prdt_cd'],
-                    'bank_logo': 'logo.png',
-                    'bank_name': company.kor_co_nm,
-                    'join_way': product['join_way'],
-                    'product_name': product['fin_prdt_nm'],
-                    'basic_rate_min': options_data['basic_rate']['min'],
-                    'basic_rate_max': options_data['basic_rate']['max'],
-                    'prime_rate_min': options_data['prime_rate']['min'],
-                    'prime_rate_max': options_data['prime_rate']['max'],
-                    'months_06': '6' in options_data['save_trm'],
-                    'months_12': '12' in options_data['save_trm'],
-                    'months_24': '24' in options_data['save_trm'],
-                    'months_36': '36' in options_data['save_trm'],
-                    'rate_type_s': 'S' in options_data['rate_type'],
-                    'rate_type_m': 'M' in options_data['rate_type'],
-                    'rsrv_type_s': 'S' in options_data['rsrv_type'],
-                    'rsrv_type_f': 'F' in options_data['rsrv_type'],
-                    'join_deny': product['join_deny'],
-                    'join_member': product['join_member']
-                }
-                products.append(product_data)
-            return Response(response_data(True, products))
+            custom_products = []
+            for product in products:
+                custom_product_data = self.set_custom_product_data(product)
+                custom_products.append(custom_product_data)
+            return Response(response_data(True, custom_products))
         else:
             """
             전체 적금상품 리스트
             GET /saving_products?offset=&limit=
             """
             serializer = SavingProductBaseSerializer(saving_products_base_queryset, many=True)
-            data = serializer.data[start_index:end_index]
+            products = serializer.data[start_index:end_index]
 
         # 적금상품 옵션
-        for product in data:
+        for product in products:
             fin_prdt_cd = product['fin_prdt_cd']
             saving_product_option_queryset = SavingProductOption.objects.all().filter(fin_prdt_cd=fin_prdt_cd)
             saving_product_option_serializer = SavingProductOptionSerializer(saving_product_option_queryset, many=True)
 
             product['options'] = saving_product_option_serializer.data
 
-        return Response(response_data(True, data))
+        return Response(response_data(True, products))
 
 
 class SavingProductDetail(APIView):
@@ -188,10 +184,10 @@ class SavingProductSearch(APIView, SavingProduct):
 
         # 쿼리
         fin_prdt_nm = request.query_params.get('fin_prdt_nm')
-        fin_co_no = request.query_params.getlist('fin_co_no')
-        intr_rate_type = request.query_params.get('intr_rate_type')
-        rsrv_type = request.query_params.get('rsrv_type')
-        save_trm = request.query_params.get('save_trm')
+        fin_co_no = request.query_params.get('fin_co_no')
+        intr_rate_type = request.query_params.getlist('intr_rate_type', None)
+        rsrv_type = request.query_params.getlist('rsrv_type', None)
+        save_trm = request.query_params.getlist('save_trm', None)
         intr_rate = request.query_params.get('intr_rate', 0)
         intr_rate2 = request.query_params.get('intr_rate2', 0)
 
@@ -203,45 +199,21 @@ class SavingProductSearch(APIView, SavingProduct):
             """
             qs = SavingProductBase.objects.all().filter(fin_prdt_nm__contains=fin_prdt_nm)
             serializer = SavingProductBaseSerializer(qs, many=True)
-            data = serializer.data[start_index:end_index]
+            products = serializer.data[start_index:end_index]
 
             # 적금상품 옵션
-            for product in data:
+            for product in products:
                 fin_prdt_cd = product['fin_prdt_cd']
                 saving_product_option_queryset = SavingProductOption.objects.all().filter(fin_prdt_cd=fin_prdt_cd)
                 saving_product_option_serializer = SavingProductOptionSerializer(saving_product_option_queryset,
                                                                                  many=True)
                 product['options'] = saving_product_option_serializer.data
 
-            products = []
-            for product in data:
-                company = CompanyBase.objects.get(fin_co_no=product['fin_co_no'])
-                options = product['options']
-                options_data = self.set_options_data(options)
-
-                product_data = {
-                    'id': product['fin_prdt_cd'],
-                    'bank_logo': 'logo.png',
-                    'bank_name': company.kor_co_nm,
-                    'join_way': product['join_way'],
-                    'product_name': product['fin_prdt_nm'],
-                    'basic_rate_min': options_data['basic_rate']['min'],
-                    'basic_rate_max': options_data['basic_rate']['max'],
-                    'prime_rate_min': options_data['prime_rate']['min'],
-                    'prime_rate_max': options_data['prime_rate']['max'],
-                    'months_06': '6' in options_data['save_trm'],
-                    'months_12': '12' in options_data['save_trm'],
-                    'months_24': '24' in options_data['save_trm'],
-                    'months_36': '36' in options_data['save_trm'],
-                    'rate_type_s': 'S' in options_data['rate_type'],
-                    'rate_type_m': 'M' in options_data['rate_type'],
-                    'rsrv_type_s': 'S' in options_data['rsrv_type'],
-                    'rsrv_type_f': 'F' in options_data['rsrv_type'],
-                    'join_deny': product['join_deny'],
-                    'join_member': product['join_member']
-                }
-                products.append(product_data)
-            return Response(response_data(True, products))
+            custom_products = []
+            for product in products:
+                custom_product_data = self.set_custom_product_data(product)
+                custom_products.append(custom_product_data)
+            return Response(response_data(True, custom_products))
 
         # 상품옵션으로 검색
         else:
@@ -250,57 +222,66 @@ class SavingProductSearch(APIView, SavingProduct):
             GET /saving_products/search?fin_co_no=&intr_rate_type=&rsrv_type=&save_trm=&intr_rate=&intr_rate2=
             """
             # 금융회사코드
-            q_fin_co_no = Q()
-            for f in fin_co_no:
-                q_fin_co_no |= Q(fin_co_no=f)
+            q_fin_co_no = Q(fin_co_no=fin_co_no)
 
-            # 저축금리유형
-            if intr_rate_type is None:
-                q_intr_rate_type = Q()
-            else:
-                q_intr_rate_type = Q(intr_rate_type=intr_rate_type)
+            # 저축금리유형(S, M)
+            q_intr_rate_type = Q()
+            if len(intr_rate_type) > 0:
+                for item in intr_rate_type:
+                    q_intr_rate_type |= Q(intr_rate_type=item)
 
-            # 적립유형
-            if rsrv_type is None:
-                q_rsrv_type = Q()
-            else:
-                q_rsrv_type = Q(rsrv_type=rsrv_type)
+            # 적립유형(S, F)
+            q_rsrv_type = Q()
+            if len(rsrv_type) > 0:
+                for item in rsrv_type:
+                    q_rsrv_type |= Q(rsrv_type=item)
+
+            # 저축기간(6, 12, 24, 36)
+            q_save_trm = Q()
+            if len(save_trm) > 0:
+                for item in save_trm:
+                    q_save_trm |= Q(save_trm=item)
+
+            # 기본금리
+            q_intr_rate = Q(intr_rate__gte=intr_rate)
+
+            # 우대금리
+            q_intr_rate2 = Q(intr_rate2__gte=intr_rate2)
 
             # 검색된 적금상품 옵션
-            qs = SavingProductOption.objects.all().filter(q_fin_co_no,
-                                                          q_intr_rate_type,
-                                                          q_rsrv_type,
-                                                          save_trm=save_trm,
-                                                          intr_rate__gte=intr_rate,
-                                                          intr_rate2__gte=intr_rate2)
+            qs = SavingProductOption.objects.filter(q_fin_co_no,
+                                                    q_intr_rate_type,
+                                                    q_rsrv_type,
+                                                    q_save_trm,
+                                                    q_intr_rate,
+                                                    q_intr_rate2)
+
             serializer = SavingProductOptionSerializer(qs, many=True)
-            options = serializer.data[start_index:end_index]
+            options = serializer.data
 
-            # 검색된 적금상품 개수
-            total_count = len(serializer.data)
-
-            # 검색된 적금상품 리스트
-            data = {
-                'total_count': total_count,
-                'max_offset_no': math.ceil(total_count / limit),
-                'now_offset_no': offset,
-                'products': []
-            }
-
-            # 검색된 적금상품 옵션의 상품명
+            # 검색된 적금상품 옵션
+            products = []
             for option in options:
-                saving_product_base_queryset = SavingProductBase.objects.get(fin_co_no=option['fin_co_no'],
-                                                                             fin_prdt_cd=option['fin_prdt_cd'])
-                saving_product_base_serializer = SavingProductBaseSerializer(saving_product_base_queryset)
+                qs = SavingProductBase.objects.get(fin_co_no=option['fin_co_no'], fin_prdt_cd=option['fin_prdt_cd'])
+                serializer = SavingProductBaseSerializer(qs)
+                product = serializer.data
 
-                # saving_product_base_serializer.data 타입이 ReturnDict 이기 때문에 key 값 추가가 안되므로 new_dict 로 복사하여 추가
-                new_dict = {}
-                new_dict.update(saving_product_base_serializer.data)
-                new_dict['option'] = option
+                exist_product = next((item for item in products if item['fin_prdt_cd'] == product['fin_prdt_cd']), False)
+                if exist_product:
+                    # 상품이 존재하면 상품에 옵션 데이터를 맵핑
+                    exist_product['options'].append(option)
+                else:
+                    # 상품이 존재하지 않으면 상품과 옵션을 맵핑한 데이터를 추가
+                    product['options'] = []
+                    product['options'].append(dict(option))
+                    products.append(product)
 
-                data['products'].append(new_dict)
-
-            return Response(data)
+            # 맵핑된 상품에서 썸네일을 위한 커스텀 데이터 생성
+            custom_products = []
+            for product in products:
+                custom_product_data = self.set_custom_product_data(product)
+                custom_products.append(custom_product_data)
+            return Response(response_data(True, custom_products))
 
 
 class SavingProductBookmarkList(APIView):
