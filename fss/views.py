8324fdce92c6cdedb5ad166ee943224e70241200
Junhong-Kim <jhk1005jhk@gmail.com>
2018-07-16 21:44:10 +0900

    Add: get saving-products

--- fss/views.py
+++ fss/views.py
@@ -1,52 +1,30 @@
 from . import services
-from multiprocessing import Process
-
-from saving.serializers import SavingProductBaseSerializer, SavingProductOptionSerializer
+from common.response import response_data
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 
-@api_view(['POST'])
+@api_view(['GET'])
 def saving_products(request):
-    if request.method == 'POST':
+    if request.method == 'GET':
+        products = []
         top_fin_grp_no = request.query_params.get('topFinGrpNo', '020000')
-        page_no = request.query_params.get('pageNo', '1')
+        page_no = request.query_params.get('pageNo', 0)
+        res = services.get_saving_products(top_fin_grp_no, page_no).json()
 
-        response = services.get_saving_products(top_fin_grp_no, page_no).json()
-        max_page_no = int(response['result']['max_page_no']) + 1
+        for page_no in range(int(res['result']['max_page_no'])):
+            res = services.get_saving_products(top_fin_grp_no, page_no + 1).json()
+            product_list = res['result']['baseList']
+            option_list = res['result']['optionList']
 
-        for now_page_no in range(1, max_page_no):
-            response = services.get_saving_products(top_fin_grp_no, now_page_no).json()
-
-            saving_products_base = response['result']['baseList']
-            saving_products_option = response['result']['optionList']
-
-            Process(target=saving_products_base_process, args=(saving_products_base, top_fin_grp_no)).start()
-            Process(target=saving_products_option_process, args=(saving_products_option,)).start()
-
-        return Response(response)
-
-
-def saving_products_base_process(saving_products_base, top_fin_grp_no):
-    for saving_product_base in saving_products_base:
-        saving_product_base['top_fin_grp_no'] = top_fin_grp_no
-        serializer = SavingProductBaseSerializer(data=saving_product_base)
-        if serializer.is_valid():
-            serializer.save()
-        else:
-            raise Exception(serializer.errors)
-    return
-
-
-def saving_products_option_process(saving_products_option):
-    for saving_product_option in saving_products_option:
-        serializer = SavingProductOptionSerializer(data=saving_product_option)
-        if serializer.is_valid():
-            serializer.save()
-        else:
-            raise Exception(serializer.errors)
-    return
+            for product in product_list:
+                product['options'] = []
+                for option in option_list:
+                    if product['fin_prdt_cd'] == option['fin_prdt_cd']:
+                        product['options'].append(option)
+                products.append(product)
+        return Response(response_data(True, products))
 
 
 @api_view(['GET'])
