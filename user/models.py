257b934ef8f35423b00c52e432c23fbf92bb001c
Junhong-Kim <jhk1005jhk@gmail.com>
2018-07-25 21:47:49 +0900

    Add: role field to user model

--- user/models.py
+++ user/models.py
@@ -2,7 +2,12 @@ from django.db import models
 
 
 class User(models.Model):
-    email = models.EmailField(max_length=128)
+    USER_ROLE = (
+        ('A', 'Admin'),
+        ('G', 'Guest')
+    )
+
+    email = models.EmailField(max_length=128, unique=True)
     name = models.CharField(max_length=64)
     gender = models.CharField(max_length=32)
     birthday = models.CharField(max_length=32)
@@ -10,6 +15,7 @@ class User(models.Model):
     fb_access_token = models.CharField(max_length=256)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    role = models.CharField(max_length=32, choices=USER_ROLE, default='G')
 
     class Meta:
         db_table = 'users'
