b8928f23aab25151fea8f6fc5ddc3b68e54a17f2
Junhong-Kim <jhk1005jhk@gmail.com>
2018-07-27 00:16:08 +0900

    Modify: user viewset to class based view

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,7 @@
-from django.urls import include, path
-from rest_framework.routers import DefaultRouter
+from django.urls import path
 from . import views
 
-router = DefaultRouter()
-router.register('', views.UserViewSet)
-
 urlpatterns = [
-    path('', include(router.urls))
+    path('', views.UserList.as_view()),
+    path('<int:pk>', views.UserDetail.as_view()),
 ]
--- user/views.py
+++ user/views.py
@@ -1,5 +1,9 @@
-from rest_framework import viewsets
+from django.http import Http404
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
+from common.response import response_data
 from user.models import User
 from user.serializers import UserSerializer
 
@@ -7,3 +11,45 @@ from user.serializers import UserSerializer
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class UserList(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(response_data(True, serializer.data))
+        else:
+            return Response(response_data(False, serializer.errors), status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request):
+        qs = User.objects.all()
+        serializer = UserSerializer(qs, many=True)
+        return Response(response_data(True, serializer.data))
+
+
+class UserDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return User.objects.get(pk=pk)
+        except User.DoesNotExist:
+            return Http404
+
+    def get(self, request, pk):
+        user = self.get_object(pk)
+        serializer = UserSerializer(user)
+        return Response(response_data(True, serializer.data))
+
+    def put(self, request, pk):
+        user = self.get_object(pk)
+        serializer = UserSerializer(user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(response_data(True, serializer.data))
+        else:
+            return Response(response_data(False, serializer.errors), status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        user = self.get_object(pk)
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
