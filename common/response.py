195b9120aacd09949ec17653d7c9ba14a19b6cda
Junhong-Kim <jhk1005jhk@gmail.com>
2018-07-31 01:35:59 +0900

    Modify: response_data

--- common/response.py
+++ common/response.py
@@ -1,20 +1,14 @@
-from rest_framework.utils.serializer_helpers import ReturnList
-
-
 def response_data(success, data=None):
-    res = {}
+    res = {'success': success}
 
-    if success and (isinstance(data, ReturnList) or isinstance(data, list)):
-        # 복수
-        res['success'] = 'YES'
-        res['total_count'] = len(data)
+    if success and isinstance(data, dict):
+        # 단수
         res['data'] = data
     elif success:
-        # 단수
-        res['success'] = 'YES'
+        # 복수
+        res['total_count'] = len(data)
         res['data'] = data
     else:
         # 에러
-        res['success'] = 'NO'
-        res['msg'] = str(data)
+        res['error'] = data
     return res
