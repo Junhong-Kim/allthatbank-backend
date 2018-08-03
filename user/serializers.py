3ee05b74790197f0f67cd3452df4ac79c16cc1ef
Junhong-Kim <jhk1005jhk@gmail.com>
2018-08-04 02:04:13 +0900

    Add: encrypt password

--- user/serializers.py
+++ user/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
+
+from allthatbank.utils.cipher import AESCipher
 from .models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        if validated_data.get('password') is not None:
+            validated_data['password'] = AESCipher().encrypt_str(validated_data['password'])
+        return User.objects.create(**validated_data)
 
     class Meta:
         model = User
