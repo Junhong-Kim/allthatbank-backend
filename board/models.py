e448d160d30eae5e7ffc9f38daf85fae38543ed0
Junhong-Kim <jhk1005jhk@gmail.com>
2018-08-24 21:45:38 +0900

    Add: free model of board app

--- board/models.py
+++ board/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from core.models import TimeStampedModel
+from user.models import User
+
+
+class Free(TimeStampedModel):
+    title = models.CharField(max_length=100)
+    contents = models.TextField()
+    views = models.IntegerField()
+    like = models.IntegerField()
+    user = models.ForeignKey(User, db_constraint=True, on_delete=models.CASCADE)
